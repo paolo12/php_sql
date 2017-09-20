eceac4b1a3c5e1e1738214e7c67c01c98fbca3d0
pavel <ppyakov@gmail.com>
2017-09-20 12:27:48 +0300

    lesson_2

---
+++ L2/lesson_2.php
@@ -0,0 +1,28 @@
+<?php
+$x = rand(0,100);
+echo "Число = ".$x;
+echo PHP_EOL;
+$y1 = $y2 = 1;
+
+if ($y1 > $x){
+	echo "Задуманное число НЕ входит в числовой ряд";
+	echo PHP_EOL;
+}else{
+    while (($y1 < $x) or ($y1 == $x)){
+	if ($y1 == $x){
+		echo "Задуманное число входит в числовой ряд";
+		echo PHP_EOL;
+		break;
+	}
+	else{
+		$y3 = $y1;
+		$y1 += $y2;
+		$y2 = $y3;
+		if ($y1 > $x){
+			echo "Задуманное число НЕ входит в числовой ряд";
+			echo PHP_EOL;
+			break;
+		}
+	}
+	}
+}
