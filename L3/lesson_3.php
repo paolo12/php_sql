4f62a436192e3c6c574cce4e6a020cf8c28e0886
paolo12 <ppyakov@gmail.com>
2017-09-27 00:17:47 +0300

    Update lesson_3.php

--- L3/lesson_3.php
+++ L3/lesson_3.php
@@ -8,34 +8,49 @@ $antarctica = ['Globicephala melas', 'Lobodon carcinophagus', 'Phocoena dioptric
 
 $animals = [$eurasia , $africa, $north_america, $south_america, $australia, $antarctica];
 
+$double_name_animals = [];
+$all_animals_name = [];
+$count_names = [];
+
 foreach($animals as $continent){
-	foreach($continent as $animal){
-		if(strpos($animal, ' ')){
-			$double_name_animals[] = $animal;
-		};
-	}
+    foreach($continent as $animal){
+        if(strpos($animal, ' ') and substr_count($animal,' ') < 2){
+            $double_name_animals[] = $animal;
+        };
+    }
 }
 
 foreach($double_name_animals as $dn_animal){
-	$all_animals_name[] = explode(" ", $dn_animal);
+    $all_animals_name[] = explode(" ", $dn_animal);
 }
 
+foreach ($all_animals_name as $i => $new_animal) {
+    ${"new_animal{$i}"} = $new_animal; 
+}
+
+foreach($double_name_animals as $aanimal){
+    print_r($aanimal);
+    echo PHP_EOL;
+}
+
+/*
 for ($i = 0; $i < count($all_animals_name); $i++) {
-	$count_names[]= $i;
+    $count_names[]= $i;
 }
 
 for ($i = 0; $i < count($all_animals_name); $i++) {
     $n = rand(0, count($all_animals_name));
-	while (in_array($n, $count_names) == false){
-		$new_animals[] = $all_animals_name[$i][0].' '.$all_animals_name[$n][1];
-		break;
-		$n = rand(0, count($all_animals_name));
-	}	
-	unset($count_names[$n]);
+    while (in_array($n, $count_names) == false){
+        $new_animals[] = $all_animals_name[$i][0].' '.$all_animals_name[$n][1];
+        break;
+        $n = rand(0, count($all_animals_name));
+    }
+    unset($count_names[$n]);
 }
 
 foreach($new_animals as $new_animal){
-	print_r($new_animal);
-	echo PHP_EOL;
+    print_r($new_animal);
+    echo PHP_EOL;
 }
+*/
 ?>
