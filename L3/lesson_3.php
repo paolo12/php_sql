65e6ae67db2dbf69465df9cd4416736d2a2653c2
paolo12 <ppyakov@gmail.com>
2017-10-01 00:51:56 +0300

    Update lesson_3.php

--- L3/lesson_3.php
+++ L3/lesson_3.php
@@ -5,52 +5,34 @@ $north_america = ['Panthera onca', 'Spilogale gracilis', 'Martes pennanti', 'Bis
 $south_america = ['Tayassu pecari', 'Nasua', 'Saimiri sciureus', 'Cuniculus paca'];
 $australia = ['Onychogalea lunata', 'Petaurus breviceps', 'Notoryctes', 'Perameles bougainville'];
 $antarctica = ['Globicephala melas', 'Lobodon carcinophagus', 'Phocoena dioptrica', 'Orcinus orca'];
+$world = [$eurasia , $africa, $north_america, $south_america, $australia, $antarctica];
 
-$animals = [$eurasia , $africa, $north_america, $south_america, $australia, $antarctica];
-
-$double_name_animals = [];
-$all_animals_name = [];
-$count_names = [];
-
-foreach($animals as $continent){
-    foreach($continent as $animal){
-        if(strpos($animal, ' ') and substr_count($animal,' ') < 2){
-            $double_name_animals[] = $animal;
-        };
-    }
+foreach($world as $continent){
+	foreach($continent as $animal){
+		if(strpos($animal, ' ') and substr_count($animal, ' ') < 2){
+			$double_name_animals[] = $animal;
+		};
+	}
 }
 
 foreach($double_name_animals as $dn_animal){
-    $all_animals_name[] = explode(" ", $dn_animal);
+	$all_animals_name[] = explode(" ", $dn_animal);
 }
 
-foreach ($all_animals_name as $i => $new_animal) {
-    ${"new_animal{$i}"} = $new_animal; 
+foreach($all_animals_name as $key => $value){
+	$first_name[] = $value[0];
+	$last_name[] = $value[1];
 }
 
-foreach($double_name_animals as $aanimal){
-    print_r($aanimal);
-    echo PHP_EOL;
+shuffle($last_name);
+
+$temp_result = array_map(null, $first_name, $last_name);
+
+foreach($temp_result as $animal){
+	$new_animals[] = implode(" ", $animal);
 }
 
-/*
-for ($i = 0; $i < count($all_animals_name); $i++) {
-    $count_names[]= $i;
+foreach($new_animals as $animal){
+	print($animal.PHP_EOL);
 }
-
-for ($i = 0; $i < count($all_animals_name); $i++) {
-    $n = rand(0, count($all_animals_name));
-    while (in_array($n, $count_names) == false){
-        $new_animals[] = $all_animals_name[$i][0].' '.$all_animals_name[$n][1];
-        break;
-        $n = rand(0, count($all_animals_name));
-    }
-    unset($count_names[$n]);
-}
-
-foreach($new_animals as $new_animal){
-    print_r($new_animal);
-    echo PHP_EOL;
-}
-*/
 ?>
